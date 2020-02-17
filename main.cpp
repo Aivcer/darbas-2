26bc4429fe4fb8108b30878ad7cf8cd0680d4848
Aivcer <58290214+Aivcer@users.noreply.github.com>
2020-02-17 23:36:05 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,144 @@
+#include <iostream>
+#include <limits>
+#include <iomanip>
+#include <algorithm>
+#include <time.h>
+
+using namespace std;
+
+int main()
+{
+    srand( time (NULL) );
+    int n, k=0, r=1;
+    string v[100], p[100];
+    double er[100], vid[100], galv[100], galm[100], med[100];
+    while( r == 1)
+    {
+        n = 0;
+        cout << "Koks jusu vardas?"<<endl;
+        cin >> v[k];
+        cout << "Kokia jusu pavarde?"<<endl;
+        cin >> p[k];
+        double *C;
+        C = new double [1];
+        int s;
+        cout << "Jei norite pazymius generuoti atsitiktinai irasykite 1, o jei vesite patys, rasykite 0" << endl;
+        cin >> s;
+        if ( s == 1)
+        {
+            n=rand()%10+1;
+            delete C;
+            C = new double [n];
+            for (int j=0; j<n; j++)
+            {
+                C[n]=rand()%10+1;
+            }
+        }
+        else
+        {
+            cout << "Kokie jusu namu darbu rezultatai? Nuo 1 iki 10. Kai suvesite visus, irasykite bet koki simboli, kuris nera nuo 1 iki 10"<<endl;
+        cin >> C[n];
+        if( C[n] < 1 || C[n] > 10 )
+        {
+            cout << "Veskite tik skaicius nuo 1 iki 10" << endl;
+            delete C;
+            n--;
+        }
+        for(;;)
+        {
+            n++;
+            double * t;
+            t = new double [n];
+            cin >> t[n];
+            if( t[n] > 0 && t[n] < 11 )
+            {
+                for(int i=0; i<n; i++)
+                    t[i] = C[i];
+                delete[] C;
+                C = new double [n+1];
+                C = t;
+                delete[] t;
+            }
+            else
+            {
+                int x;
+                cin.clear();
+                cin.ignore(numeric_limits<streamsize>::max(), '\n');
+                cout << "Turit vesti tik skaicius nuo 1 iki 10, ar noresit pridet kita pazymi? Jei taip, veskite 1, jei ne - 0" << endl;
+                cin >> x;
+                if( x == 1)
+                {
+                    cout << "Veskite tik skaicius nuo 1 iki 10" << endl;
+                    n--;
+                    cin.clear();
+                    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+                }
+                else break;
+            }
+        }
+        }
+        cout << "Kokie jusu egzamino rezultatai? Nuo 1 iki 10." << endl;
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        cin >> er[k];
+        while( er[k] < 1 || er[k] > 10 )
+        {
+            cout << "Veskite tik skaicius nuo 1 iki 10" << endl;
+            cin >> er[k];
+        }
+
+        for( int i=0; i<n; i++ )
+            vid[k] += C[i];
+        vid[k] = (double)vid[k]/n;
+        galv[k] = 0.4*vid[k] + 0.6*er[k];
+        for (int i=0; i<n-1; i++)
+        {
+            for (int j=i+1; j<n; j++)
+            {
+                if (C[i]>C[j])
+                {
+                    swap(C[i], C[j]);
+                }
+            }
+        }
+        int a, b;
+        if ( n % 2 == 0 )
+        {
+            a = n / 2;
+            b = n / 2 - 1;
+            med[k] = (C[a] + C[b]) * 1.0 / 2;
+        }
+        else
+        {
+            a = n / 2;
+            med[k] = C[a];
+        }
+        galm[k] = 0.4*med[k] + 0.6*er[k];
+        cout << "Ar norite prideti dar mokiniu? Irasykite 1, kad pridet, 0, jei tai paskutinis mokinys" << endl;
+        cin >> r;
+        if( r == 1)
+        {
+            k++;
+        }
+    }
+    if ( r == 0)
+    {
+        cout << "Jei norite naudoti vidurki galutinio balo apskaiciavimui irasykite 1. Jei norite naudoti mediana irasykite 2" <<endl;
+        cin >> r;
+        if( r == 1)
+        {
+            cout << "Pavarde            Vardas          Galutinis(Vid.)" << endl;
+            cout << "--------------------------------------------------" << endl;
+            for( int i=0; i<=k; i++)
+                cout << setw(19) << left << p[i] << setw(16) << v[i] << setw(5) << fixed << setprecision(2) << galv[i] << endl;
+        }
+        if( r == 2)
+        {
+            cout << "Pavarde            Vardas          Galutinis(Med.)" << endl;
+            cout << "--------------------------------------------------" << endl;
+            for( int i=0; i<=k; i++)
+                cout << setw(19) << left << p[i] << setw(16) << v[i] << setw(5) << fixed << setprecision(2) << galm[i] << endl;
+        }
+    }
+    return 0;
+}
