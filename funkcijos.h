a425c82baa2b6235f8bb1b8797588a8154e000f1
Aivcer <58290214+Aivcer@users.noreply.github.com>
2020-03-10 23:03:19 +0200

    Add files via upload

---
+++ funkcijos.h
@@ -0,0 +1,38 @@
+#ifndef FUNKCIJOS_H
+#define FUNKCIJOS_H
+
+#include <iostream>
+#include <limits>
+#include <iomanip>
+#include <algorithm>
+#include <time.h>
+#include <vector>
+#include <fstream>
+#include <stdexcept>
+#include <random>
+#include <chrono>
+
+using namespace std;
+
+struct duomenys
+{
+    string v; // vardas
+    string p; // pavarde
+    vector<int> C; // namu darbu pazymiu vektorius
+    double er; // egzamino rezultatas
+    double visi; //visu pazymiu suma
+    //double galv; // galutinis pazymys su vidurkiais
+    //double galm; // galutinis pazymys su mediana
+    int n; // namu darbu sk
+
+};
+
+double mediana (vector<int> X);
+double vidurkis (vector<int> A);
+bool rusiavimasV(const duomenys &a, const duomenys &b);
+bool rusiavimasP(const duomenys &a, const duomenys &b);
+void generavimas(int q, int k);
+void nuskaitymas(int &k, vector <duomenys> &d);
+void rasymas(int k, vector <duomenys> d);
+
+#endif // FUNKCIJOS_H
