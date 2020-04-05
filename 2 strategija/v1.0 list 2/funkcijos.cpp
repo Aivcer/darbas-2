940842ceeb6347518e7ecb179ef64a5a2456121c
Aivcer <58290214+Aivcer@users.noreply.github.com>
2020-04-05 23:53:25 +0300

    Add files via upload

---
+++ 2 strategija/v1.0 list 2/funkcijos.cpp
@@ -0,0 +1,145 @@
+#include "funkcijos.h"
+
+double mediana (vector<int> X)
+{
+  int a, b;
+  sort(X.begin(), X.end());
+  if (X.size()%2==0)
+  {
+    a=X.size()/2;
+    b=X.size()/2-1;
+    return (X[a]+X[b])*1.0/2;
+  }
+  else
+  {
+    a=X.size()/2;
+    return X[a];
+  }
+}
+
+double vidurkis (vector<int> A)
+{
+  int visi=0;
+  for(int i=0; i<A.size(); i++)
+  {
+    visi+=A[i];
+  }
+  return visi*1.0/A.size();
+}
+
+bool rusiavimasV(const duomenys &a, const duomenys &b)
+{
+    return a.v < b.v;
+}
+
+bool rusiavimasP(const duomenys &a, const duomenys &b)
+{
+    return a.p < b.p;
+}
+
+void generavimas(int q, int k)
+{
+    random_device rd;
+    mt19937 mt(rd());
+    uniform_int_distribution<int> dist(1, 10);
+    ofstream fq( "stud" + to_string(k) + ".txt" );
+    fq << setw(20) << left << "Vardas" << setw(20) << left << "Pavarde";
+    for (int i=0; i<q; i++)
+        fq << setw(5) << left << ( "ND" + to_string(i+1) );
+    fq << setw(5) << left << "Egz." << endl;
+    for (int i=0; i<k; i++)
+    {
+        fq << setw(20) << left << ( "Vardas" + to_string(i+1) ) << setw(20) << ( "Pavarde" + to_string(i+1) );
+        for (int j=0; j<q; j++)
+            fq << setw(5) << left  << dist(mt);
+        fq << setw(5) << left << dist(mt) << endl;
+    }
+    fq.close();
+}
+
+void nuskaitymas(int &k, list <duomenys> &d)
+{
+    auto start = std::chrono::high_resolution_clock::now();
+    ifstream in( "stud" + to_string(k) + ".txt" );
+    in.ignore(INT_MAX,'\n');
+    double h;
+    d.resize(k);
+    k=0;
+    for( list<duomenys>::iterator i=d.begin(); i!=d.end(); i++ )
+    {
+        d.push_back(duomenys());
+        (*i).n = 0;
+        in >> (*i).v;
+        in >> (*i).p;
+        while ( !in.fail() )
+        {
+            in >> h;
+            if( h > 0 && h < 11 )
+            {
+                (*i).C.push_back(h);
+                (*i).n++;
+            }
+            if (in.eof()) break;
+        }
+        (*i).n--;
+        (*i).er = (*i).C.back();
+        (*i).C.pop_back();
+        k++;
+        if (in.eof())
+        {
+            in.ignore();
+            break;
+        }
+        in.clear();
+    }
+    in.close();
+    auto end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> diff = end-start;
+    cout << k << " elementu nuskaitymas uztruko: "<< diff.count() << " s\n";
+}
+
+/*void rasymas(int k, list <duomenys>& d)
+{
+    ofstream out1("Nuskriaustukai" + to_string(k) + ".txt");
+    ofstream out2("Protuoliai" + to_string(k) + ".txt");
+    out1 << setw(20) << left << "Vardas" << setw(20) << left << "Pavarde" << setw(10) << left << "Galutinis" << endl;
+    out2 << setw(20) << left << "Vardas" << setw(20) << left << "Pavarde" << setw(10) << left << "Galutinis" << endl;
+    for( int i=0; i<k; i++)
+    {
+        if( 0.4* vidurkis(d[i].C) + 0.6*d[i].er < 5 ) out1 << setw(20) << left << d[i].v << setw(20) << d[i].p << setw(10) << fixed << setprecision(2) << 0.4* vidurkis(d[i].C) + 0.6*d[i].er << endl;
+        if( 0.4* vidurkis(d[i].C) + 0.6*d[i].er >= 5 ) out2 << setw(20) << left << d[i].v << setw(20) << d[i].p << setw(10) << fixed << setprecision(2) << 0.4* vidurkis(d[i].C) + 0.6*d[i].er << endl;
+    }
+    out1.close();
+    out2.close();
+}*/
+
+void rusiavimas(int k, list <duomenys> &d)
+{
+    auto start = std::chrono::high_resolution_clock::now();
+    list <duomenys> nus;
+    list<duomenys>::iterator it = d.begin();
+    while (it != d.end())
+    {
+        if ( (0.4*vidurkis((*it).C) + 0.6*(*it).er) < 5 )
+        {
+            nus.push_back((*it));
+            it = d.erase(it);
+        }
+        else
+            ++it;
+    }
+    auto end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> diff = end-start;
+    cout << k << " elementu rusiavimas uztruko: "<< diff.count() << " s\n";
+    //irasymas
+    ofstream out1("Nuskriaustukai" + to_string(k) + ".txt");
+    ofstream out2("Protuoliai" + to_string(k) + ".txt");
+    for (list<duomenys>::iterator i=nus.begin(); i!=nus.end(); ++i)
+        out1 << setw(20) << left << (*i).v << setw(20) << (*i).p << setw(10) << fixed << setprecision(2) << 0.4* vidurkis((*i).C) + 0.6*(*i).er << endl;
+    for (list<duomenys>::iterator i=d.begin(); i!=d.end(); ++i)
+        out2 << setw(20) << left << (*i).v << setw(20) << (*i).p << setw(10) << fixed << setprecision(2) << 0.4* vidurkis((*i).C) + 0.6*(*i).er << endl;
+    out1.close();
+    out2.close();
+    nus.clear();
+    d.clear();
+}
